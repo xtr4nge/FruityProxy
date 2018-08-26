b9eb36ad0c23e80a893f4be0a43302eb8d53dd63
xtr4nge <xtr4nge@gmail.com>
2018-08-26 13:01:58 +0100

    Installer has been updated: mitmproxy v0.18.2

--- install.sh
+++ install.sh
@@ -6,11 +6,17 @@ apt-get -y install python-pip
 
 apt-get -y install build-essential python-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev
 
+apt-get -y install libssl1.0
+apt-get -y install python-lxml
+apt-get -y build-dep -y lxml
+pip install --upgrade pyOpenSSL
+
 pip install --upgrade six
 
 #https://pypi.python.org/packages/source/u/urwid/urwid-1.3.0.tar.gz # if error, install urwid manually first. (http://urwid.org/)
 
-pip install mitmproxy
+#pip install mitmproxy
+pip install mitmproxy==0.18.2
 pip install flask
 
 echo "..DONE.."
